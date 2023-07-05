1316c63f00f007ef7f4398a5ebd47172514421c5
Taylor Bell <56656419+vtbell21@users.noreply.github.com>
2023-07-05 17:23:09 -0600

    Initial Commit

---
+++ controllers/trails.js
@@ -0,0 +1,70 @@
+const Trail = require('../models/trail');
+const { cloudinary } = require("../cloudinary");
+
+
+module.exports.index = async (req, res) => {
+    const trails = await Trail.find({});
+    res.render('trails/index', { trails })
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('trails/new');
+}
+
+module.exports.createTrail = async (req, res, next) => {
+    const trail = new Trail(req.body.trail);
+    trail.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    trail.author = req.user._id;
+    await trail.save();
+    console.log(trail);
+    req.flash('success', 'Successfully made a new trail!');
+    res.redirect(`/trails/${trail._id}`)
+}
+
+module.exports.showTrail = async (req, res,) => {
+    const trail = await Trail.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!trail) {
+        req.flash('error', 'Cannot find that trail!');
+        return res.redirect('/trails');
+    }
+    res.render('trails/show', { trail });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const trail = await Trail.findById(id)
+    if (!trail) {
+        req.flash('error', 'Cannot find that trail!');
+        return res.redirect('/trails');
+    }
+    res.render('trails/edit', { trail });
+}
+
+module.exports.updateTrail = async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    const trail = await Trail.findByIdAndUpdate(id, { ...req.body.trail });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    trail.images.push(...imgs);
+    await trail.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await trail.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
+    req.flash('success', 'Successfully updated trail!');
+    res.redirect(`/trails/${trail._id}`)
+}
+
+module.exports.deleteTrail = async (req, res) => {
+    const { id } = req.params;
+    await Trail.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted trail')
+    res.redirect('/trails');
+}
---
+++ routes/trails.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const trails = require('../controllers/trails');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateTrail } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
+const Trail = require('../models/trail');
+
+router.route('/')
+    .get(catchAsync(trails.index))
+    .post(isLoggedIn, upload.array('image'), validateTrail, catchAsync(trails.createTrail))
+
+
+router.get('/new', isLoggedIn, trails.renderNewForm)
+
+router.route('/:id')
+    .get(catchAsync(trails.showTrail))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateTrail, catchAsync(trails.updateTrail))
+    .delete(isLoggedIn, isAuthor, catchAsync(trails.deleteTrail));
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(trails.renderEditForm))
+
+
+
+module.exports = router;
